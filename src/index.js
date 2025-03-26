86ea7b84b56f0aae2aa1721240483e552a00a71e
ts-toey <thaksin.se@appworks.co.th>
2025-03-26 14:05:05 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { plus } = require("../module/plus");
+const { subtract } = require("../module/subtract");
+const { multiply } = require("../module/multiply");
+const { divide } = require("../module/divide");
+
+console.log(plus(1, 2));
+console.log(subtract(4, 2));
+console.log(multiply(10, 10));
+console.log(divide(200, 10));
---
+++ test/module.test.js
@@ -0,0 +1,20 @@
+const { plus } = require("../module/plus");
+const { subtract } = require("../module/subtract");
+const { multiply } = require("../module/multiply");
+const { divide } = require("../module/divide");
+
+test("plus 1 + 2 to equal 3", () => {
+  expect(plus(1, 2)).toBe(3);
+});
+
+test("subtract 4 - 2 to equal 2", () => {
+  expect(subtract(4, 2)).toBe(2);
+});
+
+test("multiply 10 * 10 to equal 100", () => {
+  expect(multiply(10, 10)).toBe(100);
+});
+
+test("divide 200 / 10 to equal 20", () => {
+  expect(divide(200, 10)).toBe(20);
+});
